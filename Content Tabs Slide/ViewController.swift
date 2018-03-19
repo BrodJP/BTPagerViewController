d109ab9a1bc8a31296f905f9b504b54c4f90106d
Bryan Rodriguez <brodriguez@phunware.com>
2018-03-19 16:27:27 -0600

    Initial Commit

---
+++ Content Tabs Slide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Content Tabs Slide
+//
+//  Created by Bryan Rodriguez on 3/19/18.
+//  Copyright © 2018 Applaudo Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
